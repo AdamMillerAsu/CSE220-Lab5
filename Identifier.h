b879319597de67994d287cdd33c1f2d5af5d7109
dpwong <daniel.p.wong2@gmail.com>
2014-04-28 12:53:50 -0700

    Create Identifier.h

---
+++ Identifier.h
@@ -0,0 +1,35 @@
+#ifndef __Lab5__Identifier__
+#define __Lab5__Identifier__
+
+#include <iostream>
+#include "common.h"
+#include "LineNumberList.h"
+#include "Token.h"
+
+using namespace std;
+
+/**************
+ this is a valid Pascal token.  A token must have a literal type,
+ a literal value, and a token code.
+ ***************/
+class Identifier: public Token
+{
+private:
+    literal;
+    string tokenString;
+    Identifier *leftChild;
+    Identifier *rightChild;
+    LineNumberList *list;
+    
+public:
+    Identifier();
+    ~Identifier();
+	void setLeftChild(Identifier *id);
+    Identifier *getLeftChild();
+    void setRightChild(Identifier *id);
+    Identifier *getRightChild();
+    void addToLineNumberList(LineNumberList *listItem);
+    LineNumberList *getLineNumberList();
+};
+
+#endif /* defined(__Lab5__Identifier__) */
