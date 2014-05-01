c27887b25f9f0217360b27b31cee78d7452885ff
dpwong <daniel.p.wong2@gmail.com>
2014-04-30 21:42:46 -0700

    Update LiteralType.h

--- LiteralType.h
+++ LiteralType.h
@@ -1,1 +1,20 @@
 //
+
+#ifndef __Lab5__Literal__
+#define __Lab5__Literal__
+
+#include "common.h"
+#include "Token.h"
+
+template<typename T>
+class Literal : public Token
+{ 
+protected:
+	T data;
+public:
+	Literal(T data);
+	~Literal();
+	T getLiteral();
+	setLiteral(T data);
+};
+#endif
